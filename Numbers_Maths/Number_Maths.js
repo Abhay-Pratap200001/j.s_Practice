7e1416b80ac5212b21c24c44c27bc4fd07c0c4d5
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-09-21 11:28:35 +0530

    complete the 7 practice ques of maths/number

---
+++ Numbers_Maths/Number_Maths.js
@@ -0,0 +1,54 @@
+// // Write a function to add two numbers.
+// function numsum(params) {
+//     console.log(1 + 1);    
+// }
+// numsum()
+
+
+// Subtract one number from another.
+// console.log(10 - 8);
+
+
+
+// // Multiply two numbers.
+// console.log(10 *2);
+
+
+
+// Divide two numbers and return the quotient.
+// console.log(10/2);
+
+
+// Find the remainder using the modulus operator.
+// console.log(2%20);
+
+
+// Convert a string to a number using Number().
+// console.log(typeof Number('2'));
+
+
+// Convert a number to a string using .toString().
+// let num = 2
+// let res = num.toString()
+// console.log(typeof res);
+
+
+// Round a number to the nearest integer.
+// let num = 2.2
+// let res = Math.round(num)
+// console.log(res);
+
+
+
+// Round a number up using Math.ceil().
+// let num = 2.2
+// let res = Math.ceil(num)
+// console.log(res);
+
+
+// Round a number down using Math.floor().
+// let num = 2.2
+// let res = Math.floor(num)
+// console.log(res);
+
+

29f4c8c4f4d631098569b1a6408bac23d81033b0
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-09-21 11:56:32 +0530

    complete the 1st day of practice set of date&time in j.s

---
+++ Date/Time/Dates_Times.js
@@ -0,0 +1,61 @@
+// let mydate = new Date()
+// console.log(typeof mydate);
+
+
+// let mycre = new Date(2025, 0, 23)
+// let mycre = new Date(2025, 0, 23)
+// let mycre = new Date("2025-09-06")
+// console.log(mycre.toString());
+
+// let myTimestamp = Date.now()
+// console.log(Math.floor(myTimestamp/1000));
+
+// let newDtae = new Date().getTime()
+// console.log(newDtae.toLocaleString());
+
+
+
+// Get the current date and time.
+// const currentDate = new Date();    
+// const datee = currentDate.toLocaleDateString()
+// const timee = currentDate.toLocaleTimeString()
+// const dt = currentDate.toLocaleString()
+// console.log(dt);
+
+
+
+// Get only the current year.
+// const currentyear = new Date()
+// const yer = currentyear.getFullYear()
+// console.log(yer);
+
+
+
+// Get only the current month (0-11).
+// const monthh = new Date()
+// const currentmonth = monthh.getMonth()
+// console.log(currentmonth);
+
+
+// Get the current date (1-31).
+// Get the current day of the week (0-6).
+// const datee = new Date()
+// const currentdatee = datee.getDate() 
+// const currentweek = datee.getDay().toString()
+// console.log(currentdatee, currentweek);
+
+
+
+// Get the current hour (0-23).
+// const hour = new Date()
+// const currenthour = hour.getHours()
+// console.log(currenthour);
+
+
+
+// Get the current minutes and seconds.
+//  const min = new Date()
+// const currentmin = min.getMinutes()
+// const currentsec = min.getSeconds()
+// console.log(currentmin, currentsec);
+
